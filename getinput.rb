624b73bebb98a55662bdf7dedea9edf5014f5597
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-16 17:04:45 -0400

    Getting user input

---
+++ getinput.rb
@@ -0,0 +1,9 @@
+# use gets() to get what user type
+# use chomp() to previne break line when 'enter' is press
+puts("Enter your name: ")
+name = gets().chomp()
+
+puts("How old are you?")
+age = gets().chomp()
+
+puts("Hi " + name + " you are " + age)
