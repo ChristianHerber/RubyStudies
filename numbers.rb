5b304160a80c94f872aa48111c89a068449465ca
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-16 16:51:33 -0400

    Workin with numbers

---
+++ numbers.rb
@@ -0,0 +1,30 @@
+## operations
+# sum
+puts(10 + 5)
+# subtraction
+puts(10 - 5)
+# multiplication
+puts(10 * 5)
+# division
+puts(10 / 5)
+# module
+puts(10 % 5)
+# exponentiation
+puts(10 ** 5)
+
+# with variables
+num01 = 20
+num02 = 2
+result = (num01 + num02) / 2
+puts("O resultado da conta é " + result.to_s)
+
+myNumber = 35.68745
+
+# working with numbers
+puts(myNumber.abs)
+puts(myNumber.round)
+puts(myNumber.ceil)
+puts(myNumber.floor)
+
+# Math class
+puts(Math.sqrt(myNumber))
