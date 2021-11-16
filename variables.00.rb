681434c284a4bb605f65496b7e4843f5880a0ae0
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-16 16:06:46 -0400

    add comments ;)

--- variables.00.rb
+++ variables.00.rb
@@ -1,4 +1,12 @@
+# Delclare variables
+# variable name equals variable value
+
+# this is a string variable
 name = "Chritsian"
+# this is a 
 age = 34
 
+# use puts to show de phase above in screen
+# + is usede to concatenet variables and string in ""
+# the variable age is integer, so is necessary convert to string, to can be shown in phrase
 puts("Hi! My name is " + name + " and I'm " + age.to_s + " years old.")
