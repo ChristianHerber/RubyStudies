6cd6780cd338bbc623fd3512bbab90fc8afc7e98
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-17 10:28:40 -0400

    Methods

---
+++ methods.rb
@@ -0,0 +1,9 @@
+# write the method (function) with 2 parameters
+# first parameter is a string with a default value 'no name'
+# second parameter is a integer with a default value -1
+# if anything parameter is not definid in the method call, default values will be shown
+def sayHi(name = "no name", age = -1)
+    puts("Hello " + name + ", you are " + age.to_s)
+end
+
+sayHi("Christian", 34)
