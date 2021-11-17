a608eb7880945abf67ce0a1f941f260d0c07aea0
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-17 10:03:27 -0400

    Working with hash

---
+++ hashes.rb
@@ -0,0 +1,17 @@
+# declaring the hash(object!?)
+states = {
+    "Acre" => "AC",
+    "Bahia" => "BA",
+    "Mato Grosso" => "MT",
+    "Santa Catarina" => "SC",
+    "Rio Grande do Sul" => "RS",
+}
+
+# show hash struture
+puts(states)
+
+# show a specific value 
+puts(states["Bahia"])
+
+# show hash length
+puts(states.length())
