3f043e4016e6599d53aff3fb172a63eb1e5cc27e
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-17 09:34:25 -0400

    workin with array

---
+++ arrays.rb
@@ -0,0 +1,20 @@
+# declaring a array
+names = Array["Santos", "Herber", "Christian"]
+
+# show array elements 
+puts(names)
+
+# show a especific array element by your index
+puts(names[2])
+
+# show the first element until reach the other index
+puts(names[0,2])
+
+# show the array length
+puts(names.length())
+
+# alphabetic sort array
+puts(names.sort())
+
+# from last to first
+puts(names.reverse())
