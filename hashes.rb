282cc964d0a93a5eb35acfe37b39342cef87322c
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-18 08:52:50 -0400

    use symbol in hash key

--- hashes.rb
+++ hashes.rb
@@ -1,6 +1,6 @@
 # declaring the hash(object!?)
 states = {
-    "Acre" => "AC",
+    Acre: "AC", # using symbol, object syntax
     :Bahia => "BA", # using symbol
     "Mato Grosso" => "MT",
     "Santa Catarina" => "SC",
@@ -12,6 +12,13 @@ puts(states)
 
 # show a specific value 
 puts(states[:Bahia])
+puts(states[:Acre])
 
 # show hash length
 puts(states.length())
+
+# return hash keys
+puts("Hash Keys: #{states.keys()}")
+
+# return hash values
+puts("Hash values: #{states.values()}")
