000a886c8842c911c66815de3f2b8d3d43549d86
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-18 08:43:43 -0400

    others method to include item in array

--- arrays.rb
+++ arrays.rb
@@ -3,21 +3,29 @@ names = Array["Santos", "Herber", "Christian"]
 
 # add a new array element
 names.push("Almeida")
+names << "Burgin"
+names.append("Dos")
 
 # show array elements 
-puts(names)
+puts("Array names: #{names}")
 
 # show a especific array element by your index
-puts(names[2])
+puts("3nd item: #{names[2]}")
 
 # show the first element until reach the other index
-puts(names[0,2])
+puts("Part of array: #{names[0,2]}")
 
 # show the array length
-puts(names.length())
+puts("Array length: #{names.length()}")
 
 # alphabetic sort array
-puts(names.sort())
+puts("Alphabetic sort: #{names.sort()}")
 
 # from last to first
-puts(names.reverse())
+puts("Array reverse: #{names.reverse()}")
+
+# return firs item
+puts("First item: #{names.first()}")
+
+# return last item
+puts("Last item: #{names.last()}")
